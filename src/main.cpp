264f2d1d6c45b67b34a02697368218b75635b66b
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2021-01-31 12:27:37 +0300

    exactly*2 last try

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@
 
 int main() {
     std::string s("2 + 6 * 3 / (4 - 2)");
-    std::cout << infix2postfix(s) << '\n';
-    ;  // 2 6 3 * 4 2 - / +
+    std::cout << infix2postfix(s) << '\n';  // 2 6 3 * 4 2 - / +
     s = ("(1 + 2) / (3 + 4 * 6.7) - 5.3 * 4.4");
     std::cout << infix2postfix(s);  // 1 2 + 3 4 6.7 * + / 5.3 4.4 * -
     s = "(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9";
